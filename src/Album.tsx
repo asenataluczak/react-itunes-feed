bc0ad614e2196f4a36b4aada1435644981fabdf5
asenataluczak <asenata.luczak@gmail.com>
2023-02-23 18:50:13 +0100

    added prettier and eslint config

--- src/Album.tsx
+++ src/Album.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import './Album.scss';
 
 // @ts-ignore
-function Album(props){
-
-    return (
-        <div className="test">
-            <div className="Album-index">{props.index+1}.</div>
-        <div className="Album">
-            <img src={props.image} />
-            <div>
-                <div className="Album-header">{props.name}</div>
-                <div>{props.artist}</div>
-            </div>
+function Album(props) {
+  return (
+    <div className='test'>
+      <div className='Album-index'>{props.index + 1}.</div>
+      <div className='Album'>
+        <img src={props.image} />
+        <div>
+          <div className='Album-header'>{props.name}</div>
+          <div>{props.artist}</div>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  )
 }
 
 export default Album;
--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,48 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
-import Album from "./Album";
-import axios from "axios";
+import Album from './Album';
+import axios from 'axios';
 
 function App() {
-    const [feed, setFeed] = useState<Array<any> | null>(null);
-// TODO: Add typing and interfaces everywhere
-// TODO: Extract axios to service
-// TODO: Setup prettier
-// TODO: Prepare entry object, add interface to it, pass complete needed data
+  const [feed, setFeed] = useState<Array<any> | null>(null);
+  // TODO: Add typing and interfaces everywhere
+  // TODO: Extract axios to service
+  // TODO: Prepare entry object, add interface to it, pass complete needed data
 
-    useEffect(() => {
-        axios.get(`https://itunes.apple.com/us/rss/topalbums/limit=100/json`)
-            .then(res => {
-                setFeed(res.data.feed.entry);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`https://itunes.apple.com/us/rss/topalbums/limit=100/json`)
+      .then((res) => {
+        setFeed(res.data.feed.entry);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   // @ts-ignore
-    // @ts-ignore
+  // @ts-ignore
     // @ts-ignore
     return (
-    <div className="App">
-      <header className="App-header">
-        <h1>iTunes top 100 albums</h1>
-      </header>
+      <div className='App'>
+        <header className='App-header'>
+          <h1>iTunes top 100 albums</h1>
+        </header>
         {feed && (
-            <div>
-                {feed.map((entry: any, index: number) => (
-                    <Album key={index} name={entry['im:name'].label} artist={entry['im:artist'].label} image={entry['im:image'][2].label} index={index} />
-                ))}
-            </div>
+          <div>
+            {feed.map((entry: any, index: number) => (
+              <Album
+                key={index}
+                name={entry['im:name'].label}
+                artist={entry['im:artist'].label}
+                image={entry['im:image'][2].label}
+                index={index}
+              />
+            ))}
+          </div>
         )}
-    </div>
-  );
+      </div>
+    )
 }
 
 export default App;
