3993019d2af8f097b20ada20ee9a7890bac8c9c2
asenataluczak <asenata.luczak@gmail.com>
2023-03-29 18:18:27 +0200

    added clear search bar functionality

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/App.css';
 
 interface SearchBarProps {
@@ -6,15 +6,28 @@ interface SearchBarProps {
 }
 
 function SearchBar({ search }: SearchBarProps) {
+  const [query, setQuery] = useState('');
+
+  const update = (value: string) => {
+    setQuery(value);
+    search(value);
+  };
+
   return (
+    <div className='relative my-8'>
+      {query && (
+        <button className='absolute inset-y-0 right-5 text-gray-50' onClick={() => update('')}>
+          X
+        </button>
+      )}
       <input
         type='text'
-        className='bg-gray-400 block my-8 w-full rounded-md border-0 py-0.5 px-12 text-gray-50 ring-1 ring-inset ring-gray-200 placeholder:text-gray-100 focus:ring-inset focus:ring-blue sm:text-sm sm:leading-6'
+        className='bg-gray-400 block w-full rounded-md border-0 py-0.5 px-12 text-gray-50 ring-1 ring-inset ring-gray-200 placeholder:text-gray-100 focus:ring-inset focus:ring-blue sm:text-sm sm:leading-6'
         placeholder='Search by album name or by artist'
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          search(e.target.value);
-        }}
-      />
+        value={query}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => update(e.target.value)}
+      ></input>
+    </div>
   );
 }
 
