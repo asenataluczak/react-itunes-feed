5ad5ea55c6b67388ed070bc3dc29a9940c0af76e
asenataluczak <asenata.luczak@gmail.com>
2023-04-14 21:40:45 +0200

    added addRemoveGenre()

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,24 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface FilterProps {
   genres: Array<string>;
+  filter: (genres: Array<string>) => void | undefined;
 }
 
-function Filter({ genres }: FilterProps) {
+function Filter({ genres, filter }: FilterProps) {
   const [showPanel, setShowPanel] = useState<boolean>(false);
+  const [filterOn, setFilterOn] = useState<boolean>(false);
+  const [selectedGenres, setSelectedGenres] = useState<Array<string>>([]);
+
+  const addRemoveGenre = (genre: string) => {
+    if (!selectedGenres.includes(genre)) {
+      setSelectedGenres([genre, ...selectedGenres]);
+    }
+    if (selectedGenres.includes(genre)) {
+      setSelectedGenres(selectedGenres.filter((val: string) => val !== genre));
+    }
+  };
+
+  useEffect(() => {
+    filter(selectedGenres);
+    setFilterOn(!!selectedGenres.length);
+  }, [selectedGenres]);
 
   return (
-    <div className='flex flex-col mt-7'>
+    <div className='mt-7 flex flex-col'>
       <button
-        className='relative top-4 self-center justify-self-center rounded-md border-gray-50 bg-gray-400 py-1.5 px-16 text-sm font-semibold text-gray-100 ring-1 ring-inset ring-gray-300 hover:bg-gray-300 hover:ring-gray-300'
+        className={`${
+          filterOn ? 'ring-blue' : 'ring-gray-300'
+        } relative top-4 self-center justify-self-center rounded-md bg-gray-400 py-1.5 px-16 text-sm font-semibold text-gray-100 ring-1 ring-inset hover:bg-gray-300 `}
         onClick={() => setShowPanel(!showPanel)}
       >
         Filters
       </button>
       <div
-        className={`${
-          showPanel ? 'rounded-xl border' : 'border-t'
-        } h-full border-gray-400 bg-gray-600 p-5`}
+        className={`${showPanel ? 'rounded-xl border' : 'border-t'} ${
+          filterOn ? 'border-blue' : 'border-gray-400'
+        } h-full bg-gray-600 p-5`}
       >
         {showPanel && (
           <div className='w-1/2'>
@@ -26,7 +45,12 @@ function Filter({ genres }: FilterProps) {
             {genres?.map((genre) => (
               <span
                 key={genre}
-                className='m-1 inline-block cursor-pointer rounded-2xl border border-gray-400 bg-gray-500 py-1 px-3 text-sm text-gray-100 hover:bg-gray-400'
+                className={`${
+                  selectedGenres.includes(genre)
+                    ? 'border-blue'
+                    : 'border-gray-400'
+                } m-1 inline-block cursor-pointer rounded-2xl border bg-gray-500 py-1 px-3 text-sm text-gray-100 hover:bg-gray-400`}
+                onClick={() => addRemoveGenre(genre)}
               >
                 {genre}
               </span>
