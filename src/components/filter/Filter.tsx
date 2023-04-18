e358d7c69b96ccf88f43de0145549b57dfbf394a
asenataluczak <asenata.luczak@gmail.com>
2023-04-18 21:22:48 +0200

    react-slider - in progress

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
+import ReactSlider from 'react-slider';
 
 interface FilterProps {
   genres: Array<string>;
+  prices: Array<string>;
   filter: (genres: Array<string>) => void | undefined;
 }
 
-function Filter({ genres, filter }: FilterProps) {
+function Filter({ genres, prices, filter }: FilterProps) {
   const [showPanel, setShowPanel] = useState<boolean>(false);
   const [filterOn, setFilterOn] = useState<boolean>(false);
   const [selectedGenres, setSelectedGenres] = useState<Array<string>>([]);
@@ -63,21 +65,55 @@ function Filter({ genres, filter }: FilterProps) {
         } h-full bg-gray-600 p-5`}
       >
         {showPanel && (
-          <div className='w-1/2'>
-            <div className='font-semibold text-gray-200'>Genres:</div>
-            {genres?.map((genre) => (
-              <span
-                key={genre}
-                className={`${
-                  selectedGenres.includes(genre)
-                    ? 'border-blue'
-                    : 'border-gray-400'
-                } m-1 inline-block cursor-pointer rounded-2xl border bg-gray-500 py-1 px-3 text-sm text-gray-100 hover:bg-gray-400`}
-                onClick={() => addRemoveGenre(genre)}
-              >
-                {genre}
-              </span>
-            ))}
+          <div className='flex mt-3'>
+            <div className='w-1/2'>
+              <div className='font-semibold text-gray-200'>Genres:</div>
+              {genres?.map((genre) => (
+                <span
+                  key={genre}
+                  className={`${
+                    selectedGenres.includes(genre)
+                      ? 'border-blue'
+                      : 'border-gray-400'
+                  } m-1 inline-block cursor-pointer rounded-2xl border bg-gray-500 py-1 px-3 text-sm text-gray-100 hover:bg-gray-400`}
+                  onClick={() => addRemoveGenre(genre)}
+                >
+                    {genre}
+                  </span>
+              ))}
+            </div>
+            <div className='w-1/2 ml-7'>
+              <div className='font-semibold text-gray-200 mb-9'>Price ($):</div>
+              <ReactSlider
+                className='horizontal-slider cursor-pointer mb-12'
+                thumbClassName='text-gray-50 text-center custom-thumb relative -top-8 -left-2/4 w-4 overflow-visible'
+                trackClassName='bg-gray-200 h-1 rounded'
+                min={0}
+                max={25}
+                step={0.01}
+                defaultValue={[0, 25]}
+                ariaLabel={['Lower thumb', 'Upper thumb']}
+                ariaValuetext={state => `Thumb value ${state.valueNow}`}
+                renderMark={(props) => <span {...props} />}
+                renderThumb={(props, state) => (
+                  <div {...props}>{state.valueNow}</div>
+                )}
+                pearling
+              />
+
+              {prices?.map((price) => (
+                <span
+                  key={price}
+                  className={`${
+                    selectedGenres.includes(price)
+                      ? 'border-blue'
+                      : 'border-gray-400'
+                  } m-1 inline-block cursor-pointer rounded-2xl border bg-gray-500 py-1 px-3 text-sm text-gray-100 hover:bg-gray-400`}
+                >
+                    {price}
+                  </span>
+              ))}
+            </div>
           </div>
         )}
       </div>
